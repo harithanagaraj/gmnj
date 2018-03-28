b9d33fa03942e9e5da71c809726cfb666b92f959
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 15:54:25 +0530

    prgm

---
+++ powersd.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int number;
+    int(i=0;i++;i>8);
+
+
+    printf("Enter an integer: ");  
+    
+    scanf("%d", &number);  
+    
+    printf("You entered: %d", number);
+    return 0;
+}
